f35eb05b9d83dcb59de889e44bf48954ac9a0264
Hammad740 <mohammadhammadansari91@gmail.com>
2024-07-23 11:47:01 +0530

    ejs added

--- 11_Url_Shortner/index.js
+++ 11_Url_Shortner/index.js
@@ -1,19 +1,30 @@
 import express from 'express';
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-// Import routes and connection function
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import urlRoute from './routes/url.js';
 import connectToDatabase from './connect.js';
 import Url from './models/url.js';
+
+const app = express();
+const PORT = 3000;
+
+// Set up view engine
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
 
 // Database connection (assuming connectToDatabase handles errors)
 connectToDatabase('mongodb://localhost:27017/urlShortener');
 
 // Mount routes
 app.use('/url', urlRoute);
-app.get('/:shortId', async (req, res) => {
+
+app.get('/url/:shortId', async (req, res) => {
   const shortId = req.params.shortId;
   try {
     const updatedEntry = await Url.findOneAndUpdate(
@@ -22,14 +33,19 @@ app.get('/:shortId', async (req, res) => {
     );
 
     if (!updatedEntry) {
-      return res.status(404).send('URL not found'); // Handle not found case
+      return res.status(404).send('URL not found');
     }
 
     res.redirect(updatedEntry.redirectUrl);
   } catch (error) {
     console.error('Error redirecting:', error);
-    res.status(500).send('Internal server error'); // Handle generic error
+    res.status(500).send('Internal server error');
   }
+});
+
+app.get('/testing', async (req, res) => {
+  const allUrls = await Url.find({});
+  res.render('home', { allUrls });
 });
 
 // Start the server
